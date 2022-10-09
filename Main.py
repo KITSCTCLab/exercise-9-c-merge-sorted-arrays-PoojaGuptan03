91f845d9c8ee6d3719e76038c39366d0a44823ec
PoojaGuptan03 <109951742+PoojaGuptan03@users.noreply.github.com>
2022-10-09 10:15:16 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,30 @@
 from typing import List
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+def merge(left_array: List[int], m: int, right_array: List[int], n: int) -> None:
   # Write code here
+  i=0
+  j=0
+  k=0
+
+  while i < len(left_array) and j < len(right_array):
+    if left_array[i] <= right_array[j]:
+      data[k] = left_array[i]
+      i += 1
+    else:
+      data[k] = right_array[j]
+      j += 1
+    k += 1
+
+  while i < len(left_array):
+    data[k] = left_array[i]
+    i += 1
+    k += 1
+
+  while j < len(right_array):
+     data[k]=right_array[j]
+     j += 1
+     k += 1
+
 
 
 # Do not change the following code
