2c589d6ea9061e58a50c66b9fdb9d2e9213010a4
PoojaGuptan03 <109951742+PoojaGuptan03@users.noreply.github.com>
2022-10-09 10:16:59 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@ from typing import List
 
 def merge(left_array: List[int], m: int, right_array: List[int], n: int) -> None:
   # Write code here
+  data=[]
   i=0
   j=0
   k=0
@@ -24,6 +25,7 @@ def merge(left_array: List[int], m: int, right_array: List[int], n: int) -> None
      data[k]=right_array[j]
      j += 1
      k += 1
+  return data
 
 
 
@@ -36,5 +38,5 @@ for item in input().split(', '):
   nums2.append(int(item))
 m = int(input())
 n = int(input())
-merge(nums1, m, nums2, n)
+nums1 = merge(nums1, m, nums2, n)
 print(nums1)
